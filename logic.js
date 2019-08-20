d767763b1f78d8649debd5438e298efb3c6691df
aalejandroq <45617025+aalejandroq@users.noreply.github.com>
2019-08-20 16:02:45 -0500

    Update logic.js

--- logic.js
+++ logic.js
@@ -25,7 +25,7 @@ function createFeatures(earthquakeData) {
     // Give each feature a popup describing the place and time of the earthquake
     function onEachFeature(feature, layer) {
         layer.bindPopup("<h3>" + feature.properties.place +
-            "</h3><hr><p>" + new Date(feature.properties.mag) + "</p>" +
+            "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
             "<p> Magnitude: " + feature.properties.mag + "</p>");
     }
 
